fbaf5f8a5c89c6927767e8abc99c7e5e86eea869
Marcel Balint <marcelbalint363@gmail.com>
2023-10-14 14:50:41 +0300

    Functionality to draw candles on the chart which simulates a live real stock market chart

---
+++ js/platform/helperFunctions.js
@@ -0,0 +1,39 @@
+// Generate positive numbers
+function generatePositivePriceChange() {
+  const randomNumber = Math.random();
+  return randomNumber * 20;
+}
+
+// Generate negative numbers
+function generateNegativePriceChange() {
+  const randomNumber = Math.random();
+  return -randomNumber * 20;
+}
+
+// Generate random numbers based on price range
+function generateRandomPriceChange() {
+  return (Math.random() * 2 - 1) * priceRange;
+}
+
+function drawCandle(xAxis, open, high, low, close) {
+  ctx.beginPath();
+  ctx.strokeStyle = close > open ? "limegreen" : "red";
+  ctx.lineWidth = 1;
+  // Move the drawing cursor to the top of the candlestick, which is at the high price level.
+  ctx.moveTo(xAxis, chartHeight - high);
+  // Draw a line from the top to the bottom of the candlestick, representing the price range.
+  ctx.lineTo(xAxis, chartHeight - low);
+  // Stroke the path to actually draw the lines on the canvas.
+  ctx.stroke();
+  ctx.fillStyle = close > open ? "limegreen" : "red";
+  // Draw a filled rectangle representing the candlestick body.
+  // The top and bottom of the candle are determined by the higher of the opening and closing prices
+  // and the lower of the opening and closing prices, respectively. The width is fixed at 8 pixels,
+  // and the height is the absolute difference between the opening and closing prices.
+  ctx.fillRect(
+    xAxis - candleWidth / 2,
+    chartHeight - Math.max(open, close),
+    8,
+    Math.abs(open - close)
+  );
+}
--- js/platform/script.js
+++ js/platform/script.js
@@ -1,4 +1,5 @@
 const canvas = document.getElementById("candlestickChart");
+const priceNow = document.querySelector(".price-now");
 const pricePoints = document.querySelector(".price-points");
 pricePoints.innerHTML = generatePriceLadder().join(""); // Attach the price ladder
 
@@ -9,3 +10,58 @@ function generatePriceLadder() {
     return `<p class='tick-price'>${price}</p>`;
   });
 }
+
+const ctx = canvas.getContext("2d");
+canvas.style.width = "100%";
+canvas.style.height = "100%";
+
+let candleWidth = 10;
+let chartHeight = canvas.height;
+let chartWidth = canvas.width;
+let currentPrice = 50;
+let priceRange = 10;
+
+function createCandle() {
+  const open = currentPrice;
+  let priceChange;
+  // if price goes below 10: lift the price otherwise generate random numbers
+  if (Math.floor(currentPrice) < 10) {
+    priceChange = Math.floor(generatePositivePriceChange());
+  } else if (Math.floor(currentPrice) > 140) {
+    priceChange = Math.floor(generateNegativePriceChange());
+  } else {
+    priceChange = Math.floor(generateRandomPriceChange());
+  }
+  // Calculate the high, low, and closing prices for the new candle
+  const high = open + Math.max(priceChange, 0);
+  const low = open + Math.min(priceChange, 0);
+  const close = open + priceChange;
+
+  // Remove the last candle (chart will have an auto scroll effect and follow to price)
+  candles = candles.slice(1);
+  // Add new candle
+  candles.push({ open, high, low, close });
+  currentPrice = close;
+  priceNow.textContent = Math.floor(currentPrice);
+  // Calculate the highest x-coordinate in the chart to determine if the chart needs resizing.
+  const highestX = (candles.length - 1) * candleWidth + candleWidth / 2;
+  if (highestX > chartWidth) {
+    // If highest x-coordinate goes beyond chart width, adjust chart width
+    chartWidth = highestX;
+    canvas.width = chartWidth;
+  }
+}
+
+function updateChart() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (let i = 0; i < candles.length; i++) {
+    // Calculate the x-coordinate (xAxis) for the center of the candlestick.
+    const xAxis = (i + 0.5) * candleWidth + candleWidth / 2;
+    const { open, high, low, close } = candles[i];
+    // Draw the candlestick at the specified coordinates.
+    drawCandle(xAxis, open, high, low, close);
+  }
+}
+
+setInterval(createCandle, 500);
+setInterval(updateChart, 500);
