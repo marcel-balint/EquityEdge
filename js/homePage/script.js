4c3f797e8f5ccf6285a8aac0a5191d153f372325
Marcel Balint <marcelbalint363@gmail.com>
2023-11-01 13:52:30 +0200

    Created and styled the 'description' section

--- js/homePage/script.js
+++ js/homePage/script.js
@@ -1,17 +1,21 @@
-const featuresContent = document.querySelector(".row");
+const headerElement = document.querySelector(".header-hero_text-box");
 const navigationBar = document.querySelector("nav");
 
-// Add sticky nav on scroll if features section is in window view
+// Add sticky nav on scroll if headerElement section is not in window view
 const obsCallback = function (entries) {
   const [entry] = entries;
-  entry.isIntersecting
-    ? navigationBar.classList.add("fixed-nav")
-    : navigationBar.classList.remove("fixed-nav");
+  console.log(entry);
+
+  if (!entry.isIntersecting) {
+    navigationBar.classList.add("fixed-nav");
+  } else {
+    navigationBar.classList.remove("fixed-nav");
+  }
 };
 
 const obsOptions = {
-  root: null, // All window observer
+  root: null, // Viewport
   threshold: 0, // Element in view
 };
 const observer = new IntersectionObserver(obsCallback, obsOptions);
-observer.observe(featuresContent); // Observe if 'featuresContent' is in window
+observer.observe(headerElement); // Observe if 'headerElement' is in the viewport
