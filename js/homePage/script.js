2db882824b1f12006282b52b549ff5b21bb9605e
Marcel Balint <marcelbalint363@gmail.com>
2023-11-03 20:33:45 +0200

    Functionality to toggle the dropdown menu

--- js/homePage/script.js
+++ js/homePage/script.js
@@ -1,6 +1,27 @@
 const headerElement = document.querySelector(".header-hero_text-box");
 const navigationBar = document.querySelector("nav");
 const copyrightEl = document.querySelector(".current-year");
+const dropdownInsights = document.querySelector(".insights-link");
+const dropdownAbout = document.querySelector(".about-link");
+const dropdownInsightsExpander = document.querySelector(
+  ".data-expander_insights"
+);
+const dropdownAboutExpander = document.querySelector(".data-expander_about");
+
+// Navigation dropdown links toggle
+dropdownAbout.addEventListener("click", () => {
+  dropdownInsightsExpander.classList.remove("active-dropdown");
+  dropdownAboutExpander.classList.toggle("active-dropdown");
+  dropdownAbout.classList.toggle("active-link");
+  dropdownInsights.classList.remove("active-link");
+});
+
+dropdownInsights.addEventListener("click", () => {
+  dropdownInsightsExpander.classList.toggle("active-dropdown");
+  dropdownAboutExpander.classList.remove("active-dropdown");
+  dropdownAbout.classList.remove("active-link");
+  dropdownInsights.classList.toggle("active-link");
+});
 
 const currentYear = new Date().getFullYear();
 copyrightEl.textContent = currentYear;
